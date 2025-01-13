bbb64ecef03a757a0f9578d435ce42a6d4742209
Sahabiar <sahabiar43@gmail.com>
2025-01-13 18:53:13 +0600

    some changes made

---
+++ script.js
@@ -0,0 +1,5 @@
+// Welcome to programming World
+
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+// 2. Create a variable to store the name of a current visitor to your blog. How would you name such a variable?
+// 3. Create a variable to store the number of posts you have written. How would you name such a variable?
